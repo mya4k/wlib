2307a47a35d00cf36f760b7d96f7c6f4efb0ab56
wspvlv <wspvlv@gmail.com>
2021-03-22 14:58:53 +0300

    sln is better off being static

--- inc/wc/io.h
+++ inc/wc/io.h
@@ -12,11 +12,29 @@
 
 C_DECL_BEGIN
 
+/* String length
+ * Arguments:
+ * char*	str	String
+ * 
+ * Description:
+ * Calculates the length of a given null-terminated string in characters, 
+ * including the NUL character
+ * 
+ * Method:
+ * Let "i" represent the length of the string. Check every character whether 
+ * it's NUL, if it's not NUL then "i" increases by 1, otherwise function 
+ * returns the value of "i".
+ */
+static U32 sln(char* str) {
+	U32 i = 1;
+	
+	while (str[i]) i++;
+
+	return i;
+}
+
 /* Stardard Input */
 VO	in	(char* ptr);
-
-/* String length */
-U32	sln	(char* str);
 
 C_DECL_END
 
