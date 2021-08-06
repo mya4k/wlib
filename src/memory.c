128a9d1a53a173a10da2cdca3f7c29a223a95ce9
wspvlv <wspvlv@gmail.com>
2021-08-07 00:53:44 +0300

    Memory allocation and dealloction functions

--- src/memory.c
+++ src/memory.c
@@ -11,7 +11,13 @@
 #	define MAL_SMALL_REQUEST	0xFF
 #	define MMAP_THRESHOLD		0x100000
 #	define DEV_ZERO				(int)-1
-#	define ROUND2PAGESIZE(n)	(n/pagesize + (_Bool)(n%pagesize))
+#	define ROUND2PAGESIZE(n)	((n/pagesize + (_Bool)(n%pagesize)) * pagesize)
+
+#	if PTB == 32
+#		define PTR2NEXT_2_PTR(i)	(((MChunk*)(i))->ptr2next < (_Ptr)(i) ? ((MChunk*)(i))->ptr2next & I32N : ((MChunk*)(i))->ptr2next | ((_Ptr)heap&I32N))
+#	else
+#		define PTR2NEXT_2_PTR(i)	(((MChunk*)(i))->ptr2next < (_Ptr)(i) ? ((MChunk*)(i))->ptr2next & I64N : ((MChunk*)(i))->ptr2next | ((_Ptr)heap&I64N))
+#	endif
 #endif
 
 
@@ -22,8 +28,12 @@
  * 
  */
 typedef struct MChunk {
-	Bl		allocated:1;		/* Wether the chunk is allocated */
-	_Ptr	ptr2next:(PTB-1);	/* Pointer to the next chunk*/
+	_Ptr	allocated:1;		/* Wether the chunk is allocated */
+#ifdef MCHUNK_USE_SIZE
+	_Ptr	size:(PTB-1);		/* Size */
+#else
+	_Ptr	ptr2next:(PTB-1);	/* Pointer to the next chunk */
+#endif
 	/* The rest is data */
 } MChunk;
 #endif
@@ -31,105 +41,124 @@ typedef struct MChunk {
 
 
 #ifndef REGION_VARS
-long pagesize = 0;
+static Vo* heap = NULL;		/* Start of the heap */
+static long pagesize = 0;	/* Page size */
 #endif
 
 
 
 /**
- * \brief	Allocates new pages
- * \fn		static Vo* _new_page(Msz n)
- * \param	n size in bytes
- * \return	Vo* Pointer to the new page
+ * \brief	Prepare program for dynamic memory allocation
+ * \fn		static Vo _minit()
+ * \return	Vo 
  */
-static Vo* _new_page(Msz n) {
-	/* Ask what page size is from the kernel */
-	if (!pagesize) pagesize = sysconf(_SC_PAGESIZE);
-
-	/* If we need to allocate a chunk so big that we need to use `mmap(2)` */
-	if (n >= MMAP_THRESHOLD)
-		return mmap(	(void*)0,
-						n, 
-						PROT_READ | PROT_WRITE,
-						MAP_PRIVATE,
-						DEV_ZERO,
-						0							);
-	/* Otherwise just `sbrk(2)` it */
-	else {
-		/* Exists briefly, I don't think they deserve to utilize memory */
-		const register Vo* a = sbrk(0);	/* Current break */
-		sbrk(n);
-		return a;
-	};
+static Vo _minit() {
+	/* Get the start of the heap */
+	heap = sbrk(0);
+	/* Get the page size */
+	pagesize = sysconf(_SC_PAGESIZE);
 }
-
-/**
- * \brief Creates a new memory chunk in a page
- * \b
- * \param page 
- * \param n 
- * \return Vo* Pointer to the new chunk
- * 
- * 
- */
-static Vo* _new_mchunk(Vo* page, const Msz n) {
-	const Vo* page_end = (Vo*)((_Ptr)page+pagesize);
-	/* Search for any unallocated space in the page */
-	for (; page < page_end; ) {
-		/* If there's an allocated chuck, jump over it */
-		if ( (*(MChunk*)page).allocated )
-			page = (Vo*)((_Ptr)page + (_Ptr)(sizeof(MChunk)+(*(MChunk*)page).size));
-		/* Otherwise allocate a new chunk */
-		else {
-			/* If there's not enough space for the chunk*/
-			if (((_Ptr)page_end-(_Ptr)page) < (sizeof(MChunk)+n)) return NULL;
-			/* Otherwise, make the new chunk */
-			(*(MChunk*)page).allocated = TRUE;
-			(*(MChunk*)page).ptr2next = page + sizeof(Mch) + n;
-		}
-	}
-	return NULL;	/* Looks like the page is full of chunks */
-}
-
-/**
- * \brief	Deletes a memory chunk
- * \fn		static Vo* _delete_mchunk(MChunk* mchunk)
- * \param	mchunk	Pointer to mchunk
- * \return	Vo
- */
-static Vo _delete_mchunk(MChunk* mchunk) {
-	if (mchunk->allocated)
-		mchunk->allocated = FALSE;
-}
-
-static Vo* _delete_page() {
-
-}
-
-
 
 
 
 /**
  * \brief	Allocates vitrual memory
- * \fn		Vo* mal(Msz n)
- * \param	n	bytes to allocate
+ * \fn		Vo* mal(Msz size)
+ * \param	size	bytes to allocate
  * \return	Vo*
+ * \attention Requires `_minit()`
+ * 
  */
-Vo* mal(Msz n) {
-	return NULL;
+Vo* mal(Msz size) {
+start:
+	/* Check if ready for dynamic allocation */ 
+	if (heap) {
+		/* If request has size */
+		if (size) {
+			/* When allocating memory chunks we also need space for MChunk struct */
+			size += sizeof(MChunk);
+			/* If normal size */
+			if (size < MMAP_THRESHOLD) {
+				Vo* heapEnd = sbrk(0);	/**< End of the heap */
+				_Ptr sum = 0;	/* Here will be stored summed size of all consequent free chunks */
+				_Ptr chunk = 0;	/* Pointer to the first chunk of a group of free chunks */
+				_Ptr _ptr2next;
+				/* Try finding a group of consequent unallocated MChunk big enough for us */
+				for (	
+					/* Pointer to current MChunk, starts at the beggining of the heap */
+					Vo* i = heap;
+					/* Will check all chunks until the end of the heap */
+					i < heapEnd;
+					/* Move to next chuck if no suitable chunk was found */
+					i = (Vo*)_ptr2next
+				) {
+					_ptr2next = PTR2NEXT_2_PTR(i);
+					/*  */
+					if (sum < size) {
+						/* If the chunk allocated, reset the sum pointer and keep looking*/
+						if (((MChunk*)i)->allocated) sum = 0;
+						/**/
+						else {
+							sum += _ptr2next - (_Ptr)i;
+							chunk = _ptr2next;
+						}
+					}
+					else {
+						heapEnd = ((MChunk*)chunk)->ptr2next;
+						goto end;
+					};
+				}
+
+				/* If no suitable space was found, allocate more memory */
+				chunk = sbrk(0);
+				heapEnd = sbrk(ROUND2PAGESIZE(size));
+end:			sum = chunk + size;
+				/*
+				 * Create a new memory chunk by slicing a bigger one
+				 */
+				/* Store the end of the chunk for later */
+				/* Set out new chunk as allocated */
+				((MChunk*)chunk)->allocated = TRUE;
+				/* The pointer is to the next chunk is the pointer to the remaining area of the bigger chunk */
+				((MChunk*)chunk)->ptr2next = sum;
+				/* The remaining space is turned into another chunk that is unallocated */
+				((MChunk*)sum)->allocated = FALSE;
+				((MChunk*)sum)->ptr2next = heapEnd;
+				return chunk - sizeof(MChunk);
+			}
+
+			/* If size is too big, allocate using mmap */
+			else {
+				return mmap(	(void*)0,
+								ROUND2PAGESIZE(size + sizeof(MChunk)), 
+								PROT_READ | PROT_WRITE,
+								MAP_PRIVATE | MAP_ANONYMOUS,
+								DEV_ZERO,
+								0										);
+			}
+		}
+		/* If size = 0 */
+		else return NULL;
+	}
+	/* Otherwise, prepare the program and start over this function call */ 
+	else {
+		_minit();
+		goto start;
+	}
 }
 
 /**
  * \brief	Deallocates virtual memory
  * \fn		Vo	mfr(Vo* p, Msz n)
  * \param	p 
- * \param	n 
  * \return	Vo
  */
-Vo	mfr(Vo* p, Msz n) {
+Vo	mfr(Vo* p) {
 	if (p) {
-		brk( (Vo*)((_Ptr)p-n) );
+		if (p >= heap && p <= sbrk(0)) {
+			((MChunk*)p)->allocated = FALSE;
+		}
+		else munmap(p, PTR2NEXT_2_PTR(p)-(_Ptr)p);
 	}
 	return_void;
 }
