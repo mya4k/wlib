61c8d449c344449be83717e6ebdb52f129e37c40
Wispy <wspvlv@gmail.com>
2021-10-31 01:32:50 +0300

    const manipulations + using short names

--- src/string.c
+++ src/string.c
@@ -2,16 +2,16 @@
 
 /**
  * \brief	String length
- * \fn		wl_Sl wl_sl(const char* restrict const s)
+ * \fn		Sl sl(const char* restrict const s)
  * \param	s	Pointer to string
- * \return	wl_Sl
+ * \return	Sl
  */
-wl_Sl wl_sl(char* restrict const s) {
+Sl sl(const char* restrict const s) {
 	/* 1. Creates a varible which will store the pointer to a character of 
 	 * string `s`. The parameter `s` is preserved, in order to calculate
 	 * the difference in bytes between `s` and `a` at return point.
 	 */
-	char* a;
+	const char* a;
 	/* 2. Checks each chacaracter, and if a character is NUL, breaks the loop 
 	 */
 	for (a=s; *a; a++);
