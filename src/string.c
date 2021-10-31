796c7abe10f717cb400352811543a883e89f4e5a
Wispy <wspvlv@gmail.com>
2021-10-31 18:20:16 +0300

    A more difficult but faster method

--- src/string.c
+++ src/string.c
@@ -1,5 +1,22 @@
 #include <wc/string.h>
 
+
+
+#if UMB>=64
+/* Value of `UMax`, where all indiviual bytes are `0x01` */
+#	define _LOW		0x0101010101010101
+/* Value of `UMax`, where all indiviual bytes are `0x80` */
+#	define _HIGH	0x8080808080808080
+#else
+/* Value of `UMax`, where all indiviual bytes are `0x01` */
+#	define _LOW		0x01010101
+/* Value of `UMax`, where all indiviual bytes are `0x08` */
+#	define _HIGH	0x80808080
+#endif
+/* Whether there's a byte that has a value of `0x00` */
+#define _HASZERO(X)  (((X)-_LOW) & ~(X) & _HIGH)
+
+
 /**
  * \brief	String length
  * \fn		Sl sl(const char* restrict const s)
@@ -7,17 +24,56 @@
  * \return	Sl
  */
 Sl sl(const char* restrict const s) {
-	/* 1. Creates a varible which will store the pointer to a character of 
-	 * string `s`. The parameter `s` is preserved, in order to calculate
-	 * the difference in bytes between `s` and `a` at return point.
-	 */
-	const char* a;
-	/* 2. Checks each chacaracter, and if a character is NUL, breaks the loop 
-	 */
-	for (a=s; *a; a++);
-	/* 3. Finds the length of the string by subtracting the pointer to the
-	 * beginning of the string `s` by the pointer to the NUL character &mdash;
-	 * the end of the string `s` &mdash; `a`
-	 */
-	return a-s;
+		/* 1. Creates a varible which will store the pointer to a character of 
+		* string `s`. The parameter `s` is preserved, in order to calculate
+		* the difference in bytes between `s` and `a` at return point.
+		*/
+		const char* a=s;
+
+#	ifndef WL_SL_SIMPLE
+		/* 2. Check, whether pointer `a` is 4-byte (in x86) or 8-byte (in x86-64)
+		* aligned, if not, check the character `a` points to for a NUL character
+		* if it's NUL, return `a`-`s`, otherwise, incriment `a` and repeat this
+		* step until `a` is aligned.
+		*/
+		for (; (Pt)a % sizeof(UMB); a++)
+
+		/* 3. Now that `a` is aligned. We check the value of the size of alignment
+		* for the presence of a byte that has a NUL chacaracter. If there isn't
+		* one, incriment `a` to the next alignment, if there is one or more, go
+		* to the next step.
+		*/
+		for (; !_HASZERO(*(UMax*)a); a += sizeof(UMB)) {}; /* Braces here to 
+		supress "this ‘for’ clause does not guard..." */
+
+
+		/* 4. Now we do now that, that there is a NUL character somewhere in this
+		* alignment, check each character, if it's NUL, return there difference
+		* between `s` and `a` &mdash; this is our string length.
+		*/
+#		if defined(__OPTIMIZE_SIZE__) || defined(PREFER_SIZE_OVER_SPEED)
+			for (; *a; a++);
+			return a-s;
+#		else
+#			if UMB >= 64
+				if(!a[0]) return a-s;
+				if(!a[1]) return a-s+1;
+				if(!a[2]) return a-s+2;
+				if(!a[3]) return a-s+3;
+				if(!a[4]) return a-s+4;
+				if(!a[5]) return a-s+5;
+				if(!a[6]) return a-s+6;
+				return a-s+7;
+#			else
+				if(!a[0]) return a-s;
+				if(!a[1]) return a-s+1;
+				if(!a[2]) return a-s+2;
+				return a-s+3;
+#			endif
+#		endif
+#	else
+		/* A simple lazy implimentation */
+		for (; *a; a++);
+		return a-s;
+#	endif
 }
