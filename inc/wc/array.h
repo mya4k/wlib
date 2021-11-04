e975b6fd4b197506ea47b5d039b2b6298d292a4c
Wispy <wspvlv@gmail.com>
2021-11-04 03:08:38 +0300

    Array logic implimented, up for testing

--- inc/wc/array.h
+++ inc/wc/array.h
@@ -9,21 +9,326 @@
 
 
 #ifndef WL_NO_SHORT_NAME
-#	define As	wl_As
+#	define Af		wl_Af
+#	define As		wl_As
+#	define afa		wl_afa
+#	define aas		wl_aas
+#	define ant		wl_ant
+#	define anot		wl_anot
+#	define aan		wl_aan
+#	define aand		wl_aand
+#	define aor		wl_aor
+#	define axr		wl_axr
+#	define axor		wl_axor
+#	define ann		wl_aan
+#	define anand	wl_anand
+#	define anr		wl_anr
+#	define anor		wl_anor
+#	define anx		wl_anx
+#	define anxor	wl_anxor
+#	define anol		wl_anol
+#	define antl		wl_antl
+#	define anotl	wl_anotl
+#	define aanl		wl_aanl
+#	define aandl	wl_aandl
+#	define aorl		wl_aorl
+#	define axrl		wl_axrl
+#	define axorl	wl_axorl
+#	define annl		wl_aanl
+#	define anandl	wl_anandl
+#	define anrl		wl_anrl
+#	define anorl	wl_anorl
+#	define anxl		wl_anxl
+#	define anxorl	wl_anxorl
 #	ifndef WL_NO_ALIASES
-#		define ArraySize	wl_ArraySize
+#		define ArraySize		wl_ArraySize
+#		define ArrayFunc		wl_ArrayFunc
+#		define ArrayFunction	wl_ArrayFunction
 #	endif
 #endif
 
-
-
 #ifndef WL_NO_ALIASES
-#	define wl_ArraySize	wl_As
+#	define wl_ArraySize		wl_As
+#	define wl_ArrayFunc		wl_Af
+#	define wl_ArrayFunction	wl_Af
 #endif
 
 
 
+/**
+ * \brief Array Assign
+ * \def wl_aas(a,b,size)
+ * Assigns array `b` to array `a` using `wl_afa`
+ * \return `a`
+ */
+#define wl_aas(a,b,size)	wl_afa((a),	(b),	NULL,	WL_AF_NOA,	(size))
+/**
+ * \brief Array NOT
+ * \def wl_ant(a,b,size)
+ * Compliments every bit of array `b` and assigns it to array `a`
+ * \return `a`
+ */
+#define wl_ant(a,b,size)	wl_afa((a),	(b),	NULL,	WL_AF_NTA,	(size))
+/**
+ * \brief Array OR
+ * \def wl_ant(a,b,size)
+ * Disjuncts arrays `a` and `b` and assigns it to array `r`, if `r` = NULL,
+ * a new array is allocated.
+ * \return `r`
+ */
+#define wl_aor(r,a,b,size)	wl_afa((r),	(a),	(b),	WL_AF_ORA,	(size))
+/**
+ * \brief Array AND
+ * \def wl_aan(r,a,b,size)
+ * Conjuncts arrays `a` and `b` and assigns it to array `r`, if `r` = NULL,
+ * a new array is allocated.
+ * \return `r`
+ */
+#define wl_aan(r,a,b,size)	wl_afa((r),	(a),	(b),	WL_AF_ANA,	(size))
+/**
+ * \brief Array XOR
+ * \def wl_axr(r,a,b,size)
+ * Exclusive-disjuncts arrays `a` and `b` and assigns it to array `r`, if `r` = NULL,
+ * a new array is allocated.
+ * \return `r`
+ */
+#define wl_axr(r,a,b,size)	wl_afa((r),	(a),	(b),	WL_AF_XRA,	(size))
+/**
+ * \brief Array OR
+ * \def wl_anr(r,a,b,size)
+ * Invert-disjuncts arrays `a` and `b` and assigns it to array `r`, if `r` = NULL,
+ * a new array is allocated.
+ * \return `r`
+ */
+#define wl_anr(r,a,b,size)	wl_afa((r),	(a),	(b),	WL_AF_NRA,	(size))
+/**
+ * \brief Array NAND
+ * \def wl_ann(r,a,b,size)
+ * Invert-conjuncts arrays `a` and `b` and assigns it to array `r`, if `r` = NULL,
+ * a new array is allocated.
+ * \return `r`
+ */
+#define wl_ann(r,a,b,size)	wl_afa((r),	(a),	(b),	WL_AF_NNA,	(size))
+/**
+ * \brief Array NXOR
+ * \def wl_anx(r,a,b,size)
+ * invert-exclusive-disjuncts arrays `a` and `b` and assigns it to array `r`, if `r` = NULL,
+ * a new array is allocated.
+ * \return `r`
+ */
+#define wl_anx(r,a,b,size)	wl_afa((r),	(a),	(b),	WL_AF_NXA,	(size))
+
+/**
+ * \brief Array logic NO
+ * \def wl_antl(a,b,size)
+ * Compliments every bit of array `b` and assigns it to array `a`
+ * \return `a`
+ */
+#define wl_anol(a,b,size)	wl_afab((a),	(b),	WL_AF_NTA,	(size))
+/**
+ * \brief Array logic NOT
+ * \def wl_antL(a,b,size)
+ * Compliments every bit of array `b` and assigns it to array `a`
+ * \return `a`
+ */
+#define wl_antl(a,b,size)	wl_afab((a),	(b),	WL_AF_NOA,	(size))
+/**
+ * \brief Array logic OR
+ * \def wl_aorl(a,b,size)
+ * Disjuncts arrays `a` and `b` and assigns it to array `r`, if `r` = NULL,
+ * a new array is allocated.
+ * \return `r`
+ */
+#define wl_aorl(r,a,b,size)	wl_afab((a),	(b),	WL_AF_NORA,	(size))
+/**
+ * \brief Array logic AND
+ * \def wl_aanl(r,a,b,size)
+ * Conjuncts arrays `a` and `b` and assigns it to array `r`, if `r` = NULL,
+ * a new array is allocated.
+ * \return `r`
+ */
+#define wl_aanl(r,a,b,size)	wl_afab((a),	(b),	WL_AF_NNA,	(size))
+/**
+ * \brief Array logic XOR
+ * \def wl_axrl(r,a,b,size)
+ * Exclusive-disjuncts arrays `a` and `b` and assigns it to array `r`, if `r` = NULL,
+ * a new array is allocated.
+ * \return `r`
+ */
+#define wl_axrl(r,a,b,size)	wl_afab((a),	(b),	WL_AF_NXA,	(size))
+/**
+ * \brief Array logic OR
+ * \def wl_anrl(r,a,b,size)
+ * Invert-disjuncts arrays `a` and `b` and assigns it to array `r`, if `r` = NULL,
+ * a new array is allocated.
+ * \return `r`
+ */
+#define wl_anrl(r,a,b,size)	wl_afab((a),	(b),	WL_AF_ORA,	(size))
+/**
+ * \brief Array logic NAND
+ * \def wl_annl(r,a,b,size)
+ * Invert-conjuncts arrays `a` and `b` and assigns it to array `r`, if `r` = NULL,
+ * a new array is allocated.
+ * \return `r`
+ */
+#define wl_annl(r,a,b,size)	wl_afab((a),	(b),	WL_AF_ANA,	(size))
+/**
+ * \brief Array logic NXOR
+ * \def wl_anxl(r,a,b,size)
+ * invert-exclusive-disjuncts arrays `a` and `b` and assigns it to array `r`, if `r` = NULL,
+ * a new array is allocated.
+ * \return `r`
+ */
+#define wl_anxl(r,a,b,size)	wl_afab((a),	(b),	WL_AF_XRA,	(size))
+
+/**
+ * \brief Array NOT
+ * \def wl_ant(a,b,size)
+ * Compliments every bit of array `b` and assigns it to array `a`
+ * \return `a`
+ */
+#define wl_anot(a,b,size)	wl_afa((a),	(b),	NULL,	WL_AF_NTA,	(size))
+/**
+ * \brief Array AND
+ * \def wl_aand(r,a,b,size)
+ * Conjuncts arrays `a` and `b` and assigns it to array `r`, if `r` = NULL,
+ * a new array is allocated.
+ * \return `r`
+ */
+#define wl_aand(r,a,b,size)		wl_aan(	(r),	(a),	(b),	(size)	)
+/**
+ * \brief Array XOR
+ * \def wl_axor(r,a,b,size)
+ * Exclusive-disjuncts arrays `a` and `b` and assigns it to array `r`, if `r` = NULL,
+ * a new array is allocated.
+ * \return `r`
+ */
+#define wl_axor(r,a,b,size)		wl_axr(	(r),	(a),	(b),	(size)	)
+/**
+ * \brief Array NAND
+ * \def wl_anand(r,a,b,size)
+ * Invert-conjuncts arrays `a` and `b` and assigns it to array `r`, if `r` = NULL,
+ * a new array is allocated.
+ * \return `r`
+ */
+#define wl_anand(r,a,b,size)	wl_ann(	(r),	(a),	(b),	(size)	)
+/**
+ * \brief Array OR
+ * \def wl_anor(r,a,b,size)
+ * Invert-disjuncts arrays `a` and `b` and assigns it to array `r`, if `r` = NULL,
+ * a new array is allocated.
+ * \return `r`
+ */
+#define wl_anor(r,a,b,size)		wl_anr(	(r),	(a),	(b),	(size)	)
+/**
+ * \brief Array NXOR
+ * \def wl_anxor(r,a,b,size)
+ * invert-exclusive-disjuncts arrays `a` and `b` and assigns it to array `r`, if `r` = NULL,
+ * a new array is allocated.
+ * \return `r`
+ */
+#define wl_anxor(r,a,b,size)	wl_anx(	(r),	(a),	(b),	(size)	)
+
+/**
+ * \brief Array logic NOT
+ * \def wl_antl(a,b,size)
+ * Compliments every bit of array `b` and assigns it to array `a`
+ * \return `a`
+ */
+#define wl_anotl(a,b,size)	wl_afab((a),	(b),	WL_AF_NTA,	(size))
+/**
+ * \brief Array logic AND
+ * \def wl_aandl(r,a,b,size)
+ * Conjuncts arrays `a` and `b` and assigns it to array `r`, if `r` = NULL,
+ * a new array is allocated.
+ * \return `r`
+ */
+#define wl_aandl(r,a,b,size)	wl_afab((a),	(b),	WL_AF_NNA,	(size))
+/**
+ * \brief Array logic XOR
+ * \def wl_axorl(r,a,b,size)
+ * Exclusive-disjuncts arrays `a` and `b` and assigns it to array `r`, if `r` = NULL,
+ * a new array is allocated.
+ * \return `r`
+ */
+#define wl_axorl(r,a,b,size)	wl_afab((a),	(b),	WL_AF_NXA,	(size))
+/**
+ * \brief Array logic OR
+ * \def wl_anorl(r,a,b,size)
+ * Invert-disjuncts arrays `a` and `b` and assigns it to array `r`, if `r` = NULL,
+ * a new array is allocated.
+ * \return `r`
+ */
+#define wl_anorl(r,a,b,size)	wl_afab((a),	(b),	WL_AF_ORA,	(size))
+/**
+ * \brief Array logic NAND
+ * \def wl_anandl(r,a,b,size)
+ * Invert-conjuncts arrays `a` and `b` and assigns it to array `r`, if `r` = NULL,
+ * a new array is allocated.
+ * \return `r`
+ */
+#define wl_anandl(r,a,b,size)	wl_afab((a),	(b),	WL_AF_ANA,	(size))
+/**
+ * \brief Array logic NXOR
+ * \def wl_anxorl(r,a,b,size)
+ * invert-exclusive-disjuncts arrays `a` and `b` and assigns it to array `r`, if `r` = NULL,
+ * a new array is allocated.
+ * \return `r`
+ */
+#define wl_anxorl(r,a,b,size)	wl_afab((a),	(b),	WL_AF_XRA,	(size))
+
+
+
+
+typedef enum wl_Af {
+	WL_AF_NO = 0,
+	WL_AF_AN = 1,
+	WL_AF_OR = 2,
+	WL_AF_XR = 3,
+	WL_AF_NT = 4,
+	WL_AF_NN = 5,
+	WL_AF_NR = 6,
+	WL_AF_NX = 7,
+	WL_AF_NOA = 8,
+	WL_AF_ANA = 9,
+	WL_AF_ORA = 10,
+	WL_AF_XRA = 11,
+	WL_AF_NTA = 12,
+	WL_AF_NNA = 13,
+	WL_AF_NRA = 14,
+	WL_AF_NXA = 15,
+	WL_AF_NOL = 16,
+	WL_AF_ANL = 17,
+	WL_AF_ORL = 18,
+	WL_AF_XRL = 19,
+	WL_AF_NTL = 20,
+	WL_AF_NNL = 21,
+	WL_AF_NRL = 22,
+	WL_AF_NXL = 23
+} wl_Af;
+
+/**
+ * \brief 
+ * 
+ */
 typedef U16 wl_As;
+
+
+
+EXTERN const void* wl_afa(
+	const void* restrict a, 
+	const void* restrict const b,
+	const void* restrict const c,
+	const Af func,
+	const As size
+);
+
+EXTERN Bl wl_afab(
+	const void* restrict const a, 
+	const void* restrict const b,
+	const Af func,
+	const As size
+);
 
 
 
--- src/array.c
+++ src/array.c
@@ -2,39 +2,334 @@
 
 
 
-#ifndef NO_SHORT_NAME
-#	define Af wl_Af
-#endif
-
-typedef enum wl_Af {
-	WL_AF_NO = 0,
-	WL_AF_AN = 1,
-	WL_AF_OR = 2,
-	WL_AF_XR = 3,
-	WL_AF_NT = 4,
-	WL_AF_NN = 5,
-	WL_AF_NR = 6,
-	WL_AF_NX = 7
-} wl_Af;
+#include <stdlib.h>
+#define mal malloc
 
 
 
 /**
- * \brief Array Assign
+ * \brief	
+ * \fn		static void _af1(
+				const void* restrict const a,
+				const void* restrict const b,
+				const void* restrict const c,
+				const Af func
+			)
+ * \param	a 
+ * \param	b 
+ * \param	c 
+ * \param	func wl_Af value
+ */
+static void _af1(
+	const void* restrict const a,
+	const void* restrict const b,
+	const void* restrict const c,
+	const Af func
+) {
+	switch (func) {
+	case WL_AF_NT: *((U8*)a) = ~*((U8*)a); break;
+
+	case WL_AF_AN: *((U8*)a) &= *((U8*)b); break;
+	case WL_AF_OR: *((U8*)a) |= *((U8*)b); break;
+	case WL_AF_XR: *((U8*)a) ^= *((U8*)b); break;
+	case WL_AF_NN: *((U8*)a) = ~(*((U8*)a) & *((U8*)b)); break;
+	case WL_AF_NR: *((U8*)a) = ~(*((U8*)a) | *((U8*)b)); break;
+	case WL_AF_NX: *((U8*)a) = ~(*((U8*)a) ^ *((U8*)b)); break;
+	
+	case WL_AF_NOA: *((U8*)a) = *((U8*)b); break;
+	case WL_AF_ANA: *((U8*)a) = *((U8*)b) & *((U8*)c); break;
+	case WL_AF_ORA: *((U8*)a) = *((U8*)b) | *((U8*)c); break;
+	case WL_AF_XRA: *((U8*)a) = *((U8*)b) ^ *((U8*)c); break;
+	case WL_AF_NNA: *((U8*)a) = ~(*((U8*)b) & *((U8*)c)); break;
+	case WL_AF_NRA: *((U8*)a) = ~(*((U8*)b) | *((U8*)c)); break;
+	case WL_AF_NXA: *((U8*)a) = ~(*((U8*)b) ^ *((U8*)c)); break;
+
+	default: break;
+	}
+}
+
+/**
+ * \brief	
+ * \fn		static void _af2(
+				const void* restrict const a,
+				const void* restrict const b,
+				const void* restrict const c,
+				const Af func
+			)
+ * \param	a 
+ * \param	b 
+ * \param	c 
+ * \param	func wl_Af value
+ */
+static void _af2(
+	const void* restrict const a,
+	const void* restrict const b,
+	const void* restrict const c,
+	const Af func
+) {
+	switch (func) {
+	case WL_AF_NT: *((U16*)a) = ~*((U16*)a); break;
+
+	case WL_AF_AN: *((U16*)a) &= *((U16*)b); break;
+	case WL_AF_OR: *((U16*)a) |= *((U16*)b); break;
+	case WL_AF_XR: *((U16*)a) ^= *((U16*)b); break;
+	case WL_AF_NN: *((U16*)a) = ~(*((U16*)a) & *((U16*)b)); break;
+	case WL_AF_NR: *((U16*)a) = ~(*((U16*)a) | *((U16*)b)); break;
+	case WL_AF_NX: *((U16*)a) = ~(*((U16*)a) ^ *((U16*)b)); break;
+	case WL_AF_NOA: *((U16*)a) = *((U16*)b); break;
+
+	case WL_AF_ANA: *((U16*)a) = *((U16*)b) & *((U16*)c); break;
+	case WL_AF_ORA: *((U16*)a) = *((U16*)b) | *((U16*)c); break;
+	case WL_AF_XRA: *((U16*)a) = *((U16*)b) ^ *((U16*)c); break;
+	case WL_AF_NNA: *((U16*)a) = ~(*((U16*)b) & *((U16*)c)); break;
+	case WL_AF_NRA: *((U16*)a) = ~(*((U16*)b) | *((U16*)c)); break;
+	case WL_AF_NXA: *((U16*)a) = ~(*((U16*)b) ^ *((U16*)c)); break;
+
+	default: break;
+	}
+}
+
+/**
+ * \brief	
+ * \fn		static void _af4(
+				const void* restrict const a,
+				const void* restrict const b,
+				const void* restrict const c,
+				const Af func
+			)
+ * \param	a 
+ * \param	b 
+ * \param	c 
+ * \param	func wl_Af value
+ */
+static void _af4(
+	const void* restrict const a,
+	const void* restrict const b,
+	const void* restrict const c,
+	const Af func
+) {
+	switch (func) {
+	case WL_AF_NT: *((U32*)a) = ~*((U32*)a); break;
+
+	case WL_AF_AN: *((U32*)a) &= *((U32*)b); break;
+	case WL_AF_OR: *((U32*)a) |= *((U32*)b); break;
+	case WL_AF_XR: *((U32*)a) ^= *((U32*)b); break;
+	case WL_AF_NN: *((U32*)a) = ~(*((U32*)a) & *((U32*)b)); break;
+	case WL_AF_NR: *((U32*)a) = ~(*((U32*)a) | *((U32*)b)); break;
+	case WL_AF_NX: *((U32*)a) = ~(*((U32*)a) ^ *((U32*)b)); break;
+	case WL_AF_NOA: *((U32*)a) = *((U32*)b); break;
+
+	case WL_AF_ANA: *((U32*)a) = *((U32*)b) & *((U32*)c); break;
+	case WL_AF_ORA: *((U32*)a) = *((U32*)b) | *((U32*)c); break;
+	case WL_AF_XRA: *((U32*)a) = *((U32*)b) ^ *((U32*)c); break;
+	case WL_AF_NNA: *((U32*)a) = ~(*((U32*)b) & *((U32*)c)); break;
+	case WL_AF_NRA: *((U32*)a) = ~(*((U32*)b) | *((U32*)c)); break;
+	case WL_AF_NXA: *((U32*)a) = ~(*((U32*)b) ^ *((U32*)c)); break;
+
+	default: break;
+	}
+}
+
+/**
+ * \brief	
+ * \fn		static void _af8(
+				const void* restrict const a,
+				const void* restrict const b,
+				const void* restrict const c,
+				const Af func
+			)
+ * \param	a 
+ * \param	b 
+ * \param	c 
+ * \param	func wl_Af value
+ */
+static void _af8(
+	const void* restrict const a,
+	const void* restrict const b,
+	const void* restrict const c,
+	const Af func
+) {
+	switch (func) {
+	case WL_AF_NT: *((U64*)a) = ~*((U64*)a); break;
+
+	case WL_AF_AN: *((U64*)a) &= *((U64*)b); break;
+	case WL_AF_OR: *((U64*)a) |= *((U64*)b); break;
+	case WL_AF_XR: *((U64*)a) ^= *((U64*)b); break;
+	case WL_AF_NN: *((U64*)a) = ~(*((U64*)a) & *((U64*)b)); break;
+	case WL_AF_NR: *((U64*)a) = ~(*((U64*)a) | *((U64*)b)); break;
+	case WL_AF_NX: *((U64*)a) = ~(*((U64*)a) ^ *((U64*)b)); break;
+	case WL_AF_NOA: *((U64*)a) = *((U64*)b); break;
+
+	case WL_AF_ANA: *((U64*)a) = *((U64*)b) & *((U64*)c); break;
+	case WL_AF_ORA: *((U64*)a) = *((U64*)b) | *((U64*)c); break;
+	case WL_AF_XRA: *((U64*)a) = *((U64*)b) ^ *((U64*)c); break;
+	case WL_AF_NNA: *((U64*)a) = ~(*((U64*)b) & *((U64*)c)); break;
+	case WL_AF_NRA: *((U64*)a) = ~(*((U64*)b) | *((U64*)c)); break;
+	case WL_AF_NXA: *((U64*)a) = ~(*((U64*)b) ^ *((U64*)c)); break;
+
+	default: break;
+	}
+}
+
+/**
+ * \brief	
+ * \fn		static void _af1b(
+				const void* restrict const a,
+				const void* restrict const b,
+				const void* restrict const c,
+				const Af func
+			)
+ * \param	a 
+ * \param	b 
+ * \param	func wl_Af value
+ */
+static Bl _af1b(
+	const void* restrict const a,
+	const void* restrict const b,
+	const Af func
+) {
+	switch (func) {
+	case WL_AF_NOL: return !!*((U8*)a);
+	case WL_AF_NTL: return !*((U8*)a);
+
+	case WL_AF_ANL: return *((U8*)a) && *((U8*)b);
+	case WL_AF_ORL: return *((U8*)a) || *((U8*)b);
+	case WL_AF_XRL: return	(*((U8*)a) && !*((U8*)b)) || 
+							(!*((U8*)a) && *((U8*)b));
+	case WL_AF_NNL: return !(*((U8*)a) && *((U8*)b));
+	case WL_AF_NRL: return !(*((U8*)a) || *((U8*)b));
+	case WL_AF_NXL: return	(*((U8*)a) && *((U8*)b)) || 
+							(!*((U8*)a) && !*((U8*)b));
+
+	default: return FALSE;
+	}
+}
+
+/**
+ * \brief	
+ * \fn		static void _af2b(
+				const void* restrict const a,
+				const void* restrict const b,
+				const void* restrict const c,
+				const Af func
+			)
+ * \param	a 
+ * \param	b
+ * \param	func wl_Af value
+ */
+static Bl _af2b(
+	const void* restrict const a,
+	const void* restrict const b,
+	const Af func
+) {
+	switch (func) {
+	case WL_AF_NOL: return !!*((U16*)a);
+	case WL_AF_NTL: return !*((U16*)a);
+
+	case WL_AF_ANL: return *((U16*)a) && *((U16*)b);
+	case WL_AF_ORL: return *((U16*)a) || *((U16*)b);
+	case WL_AF_XRL: return	(*((U16*)a) && !*((U16*)b)) || 
+							(!*((U16*)a) && *((U16*)b));
+	case WL_AF_NNL: return !(*((U16*)a) && *((U16*)b));
+	case WL_AF_NRL: return !(*((U16*)a) || *((U16*)b));
+	case WL_AF_NXL: return	(*((U16*)a) && *((U16*)b)) || 
+							(!*((U16*)a) && !*((U16*)b));
+
+	default: return FALSE;
+	}
+}
+
+/**
+ * \brief	
+ * \fn		static void _af4b(
+				const void* restrict const a,
+				const void* restrict const b,
+				const void* restrict const c,
+				const Af func
+			)
+ * \param	a 
+ * \param	b 
+ * \param	func wl_Af value
+ */
+static Bl _af4b(
+	const void* restrict const a,
+	const void* restrict const b,
+	const Af func
+) {
+	switch (func) {
+	case WL_AF_NOL: return !!*((U32*)a);
+	case WL_AF_NTL: return !*((U32*)a);
+
+	case WL_AF_ANL: return *((U32*)a) && *((U32*)b);
+	case WL_AF_ORL: return *((U32*)a) || *((U32*)b);
+	case WL_AF_XRL: return	(*((U32*)a) && !*((U32*)b)) || 
+							(!*((U32*)a) && *((U32*)b));
+	case WL_AF_NNL: return !(*((U32*)a) && *((U32*)b));
+	case WL_AF_NRL: return !(*((U32*)a) || *((U32*)b));
+	case WL_AF_NXL: return	(*((U32*)a) && *((U32*)b)) || 
+							(!*((U32*)a) && !*((U32*)b));
+
+	default: return FALSE;
+	}
+}
+
+/**
+ * \brief	
+ * \fn		static void _af8b(
+				const void* restrict const a,
+				const void* restrict const b,
+				const void* restrict const c,
+				const Af func
+			)
+ * \param	a 
+ * \param	b
+ * \param	func wl_Af value
+ */
+static Bl _af8b(
+	const void* restrict const a,
+	const void* restrict const b,
+	const Af func
+) {
+	switch (func) {
+	case WL_AF_NOL: return !!*((U64*)a);
+	case WL_AF_NTL: return !*((U64*)a);
+
+	case WL_AF_ANL: return *((U64*)a) && *((U64*)b);
+	case WL_AF_ORL: return *((U64*)a) || *((U64*)b);
+	case WL_AF_XRL: return	(*((U64*)a) && !*((U64*)b)) || 
+							(!*((U64*)a) && *((U64*)b));
+	case WL_AF_NNL: return !(*((U64*)a) && *((U64*)b));
+	case WL_AF_NRL: return !(*((U64*)a) || *((U64*)b));
+	case WL_AF_NXL: return	(*((U64*)a) && *((U64*)b)) || 
+							(!*((U64*)a) && !*((U64*)b));
+
+	default: return FALSE;
+	}
+}
+
+
+
+/**
+ * \brief Array Function Assign
  * 
  * \param a 
  * \param b 
+ * \param c
+ * \param func
  * \param size 
- * \return void* 
+ * \return const void* 
  */
-void* aas(
+const void* afa(
 	const void* restrict a,
 	const void* restrict const b,
-	const As size,
-	const Af func
+	const void* restrict const c,
+	const Af func,
+	const As size
 ) {
 	/* 1. Create a variable that will keep the pointer increment value. */
 	As i = 0;
+
+	if (!a) a = mal(size);
 
 #	ifndef WL_AAS_SIMPLE
 #		if IMB >= 64
@@ -42,7 +337,7 @@ void* aas(
 			 * `a` and `b`, until there are less than 8 bytes left in the array
 			 * `a` or `b`.
 			 */
-			for (; i < size&-8; i+=8) _af28(a,b);
+			for (; i < (size&-8); i+=8) _af8(a,b,c,func);
 
 			/* 3. If there are less than 8 bytes remaining, we can apply FUNC 
 			 * on them in 3 steps.
@@ -53,15 +348,15 @@ void* aas(
 			 * 	-	after previous step, if there's a byte remaining, apply 
 			 * 		FUNC to the last byte;
 			 */
-			if (size&4) { _af24(a,b); i+=4; }
-			if (size&2) { _af22(a,b); i+=2; }
-			if (size&1) { _af21(a,b); i+=1; }
+			if (size&4) { _af4(a,b,c,func); i+=4; }
+			if (size&2) { _af2(a,b,c,func); i+=2; }
+			if (size&1) { _af1(a,b,c,func); i+=1; }
 #		else
 			/* 2. The widest type is 4 bytes. Apply FUNC to every 4 bytes of 
 			 * `a` and `b`, until there are less than 4 bytes left in the array
 			 * `a` or `b`.
 			 */
-			for (; i < size&-4; i+=4) _af24(a,b);
+			for (; i < (size&-4); i+=4) _af4(a,b,c,func);
 
 			/* 3. If there are less than 4 bytes remaining, we can apply FUNC 
 			 * on them in 2 steps.
@@ -70,12 +365,65 @@ void* aas(
 			 * 	-	after previous step, if there's a byte remaining, apply 
 			 * 		FUNC to the last byte;
 			 */
-			if (size&2) { _af22(a,b); i+=2; }
-			if (size&1) { _af21(a,b); i+=1; }
+			if (size&2) { _af2(a,b,c,func); i+=2; }
+			if (size&1) { _af1(a,b,c,func); i+=1; }
 #		endif
 #	else
 		/* 2. Apply FUNC to every byte of `a` and `b` */
-		for (; i < size; i++) _af21(a,b);
+		for (; i < size; i++) _af1(a,b,c,func);
 #	endif
 		return a;
+}
+
+Bl afab(
+	const void* restrict const a,
+	const void* restrict const b,
+	const Af func,
+	const As size
+) {
+	/* 1. Create a variable that will keep the pointer increment value. */
+	As i = 0;
+
+#	ifndef WL_AAS_SIMPLE
+#		if IMB >= 64
+			/* 2. The widest type is 8 bytes. Apply FUNC to every 8 bytes of 
+			 * `a` and `b`, until there are less than 8 bytes left in the array
+			 * `a` or `b`.
+			 */
+			for (; i < (size&-8); i+=8) if (_af8b(a,b,func)) return FALSE;
+
+			/* 3. If there are less than 8 bytes remaining, we can apply FUNC 
+			 * on them in 3 steps.
+			 * 	-	if more than or equal 2 bytes remaining, apply FUNC to 2
+			 * 		bytes;
+			 * 	-	after previous step, if more than 2 bytes remaining, apply
+			 * 		FUNC to 2 bytes;
+			 * 	-	after previous step, if there's a byte remaining, apply 
+			 * 		FUNC to the last byte;
+			 */
+			if (size&4) { if (_af4b(a,b,func)) return FALSE; i+=4; }
+			if (size&2) { if (_af2b(a,b,func)) return FALSE; i+=2; }
+			if (size&1) { if (_af1b(a,b,func)) return FALSE; i+=1; }
+#		else
+			/* 2. The widest type is 4 bytes. Apply FUNC to every 4 bytes of 
+			 * `a` and `b`, until there are less than 4 bytes left in the array
+			 * `a` or `b`.
+			 */
+			for (; i < (size&-4); i+=4) if (_af4b(a,b,func)) return FALSE;
+
+			/* 3. If there are less than 4 bytes remaining, we can apply FUNC 
+			 * on them in 2 steps.
+			 * 	-	after previous step, if more than or equal 2 bytes 
+			 * 		remaining, apply FUNC to 2 bytes;
+			 * 	-	after previous step, if there's a byte remaining, apply 
+			 * 		FUNC to the last byte;
+			 */
+			if (size&2) { if (_af2b(a,b,func)) return FALSE; i+=2; }
+			if (size&1) { if (_af1b(a,b,func)) return FALSE; i+=1; }
+#		endif
+#	else
+		/* 2. Apply FUNC to every byte of `a` and `b` */
+		for (; i < size; i++) if (_af1b(a,b,func)) return FALSE;
+#	endif
+		return TRUE;
 }
