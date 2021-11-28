b80a836557dafcee444eaa359f4ec0049a51c612
wspvlv <wspvlv@gmail.com>
2021-11-28 23:29:44 +0300

    Made array.h more readable

--- inc/wc/array.h
+++ inc/wc/array.h
@@ -8,94 +8,126 @@
 
 
 
-#ifndef WL_NO_SHORT_NAME
-#	define Af		wl_Af
-#	define As		wl_As
-#	define Asf		wl_Asf
-#	define afa		wl_afa
-#	define afb		wl_afb
+#if !WL_PREFIX
+#	define ano		wl_ano
 #	define aas		wl_aas
 #	define ant		wl_ant
 #	define anot		wl_anot
+#	define aor		wl_aor
 #	define aan		wl_aan
 #	define aand		wl_aand
-#	define aor		wl_aor
 #	define axr		wl_axr
 #	define axor		wl_axor
-#	define ann		wl_aan
-#	define anand	wl_anand
 #	define anr		wl_anr
 #	define anor		wl_anor
+#	define ann		wl_ann
+#	define anand	wl_anand
 #	define anx		wl_anx
 #	define anxor	wl_anxor
-#	define anol		wl_anol
-#	define antl		wl_antl
-#	define anotl	wl_anotl
-#	define aanl		wl_aanl
-#	define aandl	wl_aandl
-#	define aorl		wl_aorl
-#	define axrl		wl_axrl
-#	define axorl	wl_axorl
-#	define annl		wl_aanl
-#	define anandl	wl_anandl
-#	define anrl		wl_anrl
-#	define anorl	wl_anorl
-#	define anxl		wl_anxl
-#	define anxorl	wl_anxorl
+
+#	define anol		wl_ano
+#	define antl		wl_ant
+#	define anotl	wl_anot
+#	define aorl		wl_aor
+#	define aanl		wl_aan
+#	define aandl	wl_aand
+#	define axrl		wl_axr
+#	define axorl	wl_axor
+#	define anrl		wl_anr
+#	define anorl	wl_anor
+#	define annl		wl_ann
+#	define anandl	wl_anand
+#	define anxl		wl_anx
+#	define anxorl	wl_anxor
+
 #	define aeq		wl_aeq
 #	define anq		wl_anq
+
+#	define Af		wl_Af
+#	define As		wl_As
+#	define Asf		wl_Asf
+
+#	define afa		wl_afa
+#	define afb		wl_afb
 #	define _anol	wl__anol
+#	define afl		wl_afl
 #	define asb		wl_asb
 #	define asa		wl_asa
-#	ifndef WL_NO_ALIASES
-#		define ArraySize		wl_ArraySize
-#		define ArrayFunc		wl_ArrayFunc
-#		define ArrayFunction	wl_ArrayFunction
-#		define ArraySearchFlags	wl_ArraySearchFlags
+
+#	if	WL_ALIASES
+#		define acompl	wl_acompl
 #	endif
 #endif
 
-#ifndef WL_NO_ALIASES
-#	define wl_ArraySize			wl_As
-#	define wl_ArrayFunc			wl_Af
-#	define wl_ArrayFunction		wl_Af
-#	define wl_ArraySearchFlags	wl_Asf
-#	define WL_POINTERDIFFERENCE	WL_PTRDIFF
+#if	WL_ALIASES
+#	define wl_acompl	wl_ant
 #endif
 
 
+/*
+ *	ARRAY BITWISE FUNCTIONS
+ */
+/* Array Bitwise No Operation [r = a] */
+#define wl_ano(r,a,size)		wl_afa((r),	(a),	WL_NULL,	WL_AF_NOA,	(size))
+/* Array Assign [a = b] */
+#define wl_aas(a,b,size)		wl_ano(a,b,size)
+/* Array Bitwise NOT [a = ~b] */
+#define wl_ant(r,a,size)		wl_afa((r),	(a),	WL_NULL,	WL_AF_NTA,	(size))
+#define wl_anot					wl_ant
+/* Array Bitwise OR [r = a|b] */
+#define wl_aor(r,a,b,size)		wl_afa((r),	(a),	(b),	WL_AF_ORA,	(size))
+/* Array Bitwise AND [r = a&b] */
+#define wl_aan(r,a,b,size)		wl_afa((r),	(a),	(b),	WL_AF_ANA,	(size))
+#define wl_aand					wl_and
+/* Array Bitwise XOR [r = a^b] */
+#define wl_axr(r,a,b,size)		wl_afa((r),	(a),	(b),	WL_AF_XRA,	(size))
+#define wl_axor					wl_axr
+/* Array Bitwise NOR [r = ~(a|b)] */
+#define wl_anr(r,a,b,size)		wl_afa((r),	(a),	(b),	WL_AF_NRA,	(size))
+#define wl_anor					wl_anr
+/* Array Bitwise NAND [r = ~(a&b)] */
+#define wl_ann(r,a,b,size)		wl_afa((r),	(a),	(b),	WL_AF_NNA,	(size))
+#define wl_anand				wl_ann
+/* Array Bitwise NXOR [r = ~(a^b)] */
+#define wl_anx(r,a,b,size)		wl_afa((r),	(a),	(b),	WL_AF_NXA,	(size))
+#define wl_anxor				wl_anx
 
-#define wl_aas(a,b,size)	wl_afa((a),	(b),	NULL,	WL_AF_NOA,	(size))
-#define wl_ant(r,a,size)	wl_afa((r),	(a),	NULL,	WL_AF_NTA,	(size))
-#define wl_aor(r,a,b,size)	wl_afa((r),	(a),	(b),	WL_AF_ORA,	(size))
-#define wl_aan(r,a,b,size)	wl_afa((r),	(a),	(b),	WL_AF_ANA,	(size))
-#define wl_axr(r,a,b,size)	wl_afa((r),	(a),	(b),	WL_AF_XRA,	(size))
-#define wl_anr(r,a,b,size)	wl_afa((r),	(a),	(b),	WL_AF_NRA,	(size))
-#define wl_ann(r,a,b,size)	wl_afa((r),	(a),	(b),	WL_AF_NNA,	(size))
-#define wl_anx(r,a,b,size)	wl_afa((r),	(a),	(b),	WL_AF_NXA,	(size))
-
+/*
+ *	ARRAY LOGICAL FUNCTIONS
+ */
+/* Array Logical No Operation [!!a] or [(Bl)a] */
 #define wl_anol(a,size)		wl__anol(a,size,WL_TRUE)
+/* Array Logical NOT [!a] */
 #define wl_antl(a,size)		wl__anol(a,size,WL_FALSE)
-
+/* Array Logical OR [a||b] */
 #define wl_aorl(a,b,size)	(wl_anol(a,size)	||	wl_anol(b,size))
+/* Array Logical AND [a||b] */
 #define wl_aanl(a,b,size)	(wl_anol(a,size)	&&	wl_anol(b,size))
+#define wl_aandl			wl_aanl
+/* Array Logical XOR */
 #define wl_axrl(a,b,size)	(wl_anol(a,size)	^	wl_anol(b,size))
+#define wl_axorl			wl_axrl
+/* Array Logical NOR */
 #define wl_anrl(a,b,size)	(wl_antl(a,size)	&&	wl_antl(b,size))
+#define wl_anorl			wl_anrl
+/* Array Logical NAND */
 #define wl_annl(a,b,size)	(wl_antl(a,size)	||	wl_antl(b,size))
+#define wl_anandl			wl_annl
+/* Array Logical NXOR */
 #define wl_anxl(a,b,size)	(wl_anol(a,size)	^	wl_antl(b,size))
+#define wl_anxorl			wl_anxl
 
+/*
+ *	ARRAY COMPARISON FUNCTIONS
+ */
+/* Array Equal [a==b] */
 #define wl_aeq(a,b,size)	wl_antl(wl_axr(NULL,	(a),	(b),	(size)	), (size))
+/* Array Not Equal [a!=b] */
 #define wl_anq(a,b,size)	wl_anol(wl_axr(NULL,	(a),	(b),	(size)	), (size))
 
 
 
-
-/**
- * \brief	Array Functions
- * \enum	wl_Af
- * \typedef	wl_Af
- * 	
- */
+/* Array Functions */
 typedef enum wl_Af {
 	WL_AF_NO = 0,	/**< Bitwise no operation */
 	WL_AF_AN = 1,	/**< Bitwise AND */
@@ -122,31 +154,16 @@ typedef enum wl_Af {
 	WL_AF_NRL = 22, /**< Logic NOR */
 	WL_AF_NXL = 23 	/**< Logic NXOR */
 } wl_Af;
-
-/**
- * \brief	Search Flags
- * \enum	wl_Asf
- * \typedef	wl_Asf
- * 
- * 
- */
+/* The maximal size of the array that array functions support. */
+typedef U16 wl_As;
+/* Search Flags */
 typedef enum wl_Asf {
-	WL_ASF_OFFSET = 0,		
-	/**< Return the offset from base pointer */
+	WL_ASF_OFFSET = 0,		/**< Return the offset from base pointer */
 	WL_ASF_NORMAL = 0,		/**< Searching from beginning to end */
 	WL_ASF_REVERSED = 1,	/**< Searching from end to beggining */
-	WL_ASF_PTRDIFF = 2,		
-	/**< Return pointer difference from the base pointer */
+	WL_ASF_PTRDIFF = 2,		/**< Return pointer difference from the base pointer */
 	WL_ASF_COUNT = 4		/**< Return the count of occurrences */
 } wl_Asf;
-
-/**
- * \brief	Array Size
- * \typedef wl_As
- * 
- * The maximal size of the array that array functions support.
- */
-typedef U16 wl_As;
 
 
 
@@ -155,44 +172,46 @@ EXTERN const void* wl_afa(
 	const void* restrict a, 
 	const void* restrict const b,
 	const void* restrict const c,
-	const Af func,
-	const As size
+	const wl_Af func,
+	const wl_As size
 );
 
 /* Array Function Boolean */
 EXTERN Bl wl_afb(
 	const void* restrict const a, 
 	const void* restrict const b,
-	const Af func,
-	const As size
+	const wl_Af func,
+	const wl_As size
 );
 
 /* Array Logical No Operation/NOT */
-EXTERN Bl wl__anol(const void* a, As size, Bl r);
+EXTERN Bl wl__anol(const void* a, wl_As size, Bl r);
 
 /* Array Fill */
 EXTERN const void* wl_afl(
 	const void* restrict const a,
-	const As sa,
+	const wl_As sa,
 	const void* restrict const b,
-	const As sb
+	const wl_As sb
 );
 
 /* Array Search Byte */
 EXTERN Pt wl_asb(
 	const void* restrict const	a,
-	As							sa,
+	wl_As						sa,
 	const char					b,
-	const Asf					flags
+	const wl_Asf				flags
 );
 
 /* Array Search Array */
 EXTERN Pt wl_asa(
 	const void* restrict const	_a,
-	As							sa,
+	wl_As						sa,
 	const void*					b,
-	As							sb,
-	const Asf					flags
+	wl_As						sb,
+	const wl_Asf				flags
 );
+
+
 
 #endif
