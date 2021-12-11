ec0116f8f380fac02b3ff2311b22d851fc5ce4ea
wspvlv <wspvlv@gmail.com>
2021-12-11 23:55:12 +0300

    Basic error checking proto

---
+++ inc/wc/error.h
@@ -0,0 +1,30 @@
+#ifndef WC_ERROR_H
+#define WC_ERROR_H
+
+
+
+#if !WL_PREFIX
+#	define ERNULL	WL_ERNULL
+#	define WRNULL	WL_WRNULL
+
+#	define err		wl_err
+#	define wrn		wl_wrn
+#endif
+
+
+
+#define WL_ERNULL	"Null pointer"
+#define WL_WRNULL	"Null pointer, allocating"
+
+
+
+#if WL_ERROR
+#	include <stdio.h>
+#	define wl_err(SRC, MSG)	printf("%s: %s\n", #SRC, MSG)
+#	define wl_wrn(SRC, MSG)	printf("%s: %s\n", #SRC, MSG)
+#else
+#	define wl_err(SRC, MSG)
+#	define wl_wrn(SRC, MSG)
+#endif
+
+#endif
