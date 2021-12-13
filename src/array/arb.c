f446e94655b88581971b9fdb6b7b1491f1e91e8e
wspvlv <wspvlv@gmail.com>
2021-12-14 00:53:00 +0300

    Error checking

--- src/array/arb.c
+++ src/array/arb.c
@@ -1,8 +1,10 @@
 #include <wc/array.h>
+#include <wc/error.h>
+
 
 
 /* Array Replace Bit */
-/* Replace all occurances of byte `b` with `r` in array `a` */
+/* Replace all occurrences of byte `b` with `r` in array `a` */
 void arb(
 	const void* restrict const a,
 	const As sa,
@@ -10,12 +12,24 @@ void arb(
 	const char r,
 	const Asf flags
 ) {
-	Pt z;
-	/* ASF_COUNT will replace all needles */
-	if (flags&ASF_COUNT)
-		while (( z = asb(a,sa,b,0) )) {
-			*(char*)z = r; z++;
- 		}
-	/* ASF_REVERSE will replace the last needle, and ASF_NORMAL the first */
-	else if (( z = asb(a,sa,b,flags&WL_ASF_REVERSED) )) *(char*)z = r;
+	if (a) {
+		Pt z;
+		/* ASF_COUNT will replace all needles */
+#if WL_ERROR
+		if (flags&ASF_COUNT)
+			if (sa)
+				while (( z = asb(a,sa,b,0) )) {
+					*(char*)z = r; z++;
+				}
+			else err(arb,ERZERO);
+#else
+		if (flags&ASF_COUNT && sa)
+			while (( z = asb(a,sa,b,0) )) {
+				*(char*)z = r; z++;
+			}
+#endif
+		/* ASF_REVERSE will replace the last needle, and ASF_NORMAL the first */
+		else if (( z = asb(a,sa,b,flags&WL_ASF_REVERSED) )) *(char*)z = r;
+	}
+	else err(arb,ERNULL);
 }
