6b6f53ebd6be1932626c15fbca2c645299e141bb
Wispy <wspvlv@gmail.com>
2021-12-16 13:00:47 +0300

    Started documenting the code

--- src/array/ara.c
+++ src/array/ara.c
@@ -9,15 +9,24 @@ void ara(
 	const As sr,
 	const Asf flags
 ) {
+/*	1. Make sure there's a haystack and a needle, otherwise return ERNULL.
+ *	2. Make sure size of a haystack is specified, otherwise return ERZERO.
+ *	3. If ASF_COUNT flag is set, replace every occurance of `*b` with `*r`:
+ *	4. Else, get the pointer to the first/last occurance of `*b`
+ *		4a. Check if `sb` and `sr` are equal; we can just assign `r` to
+ *		`b` if they are.
+ *		4b. Otherwise, split `a` into 3 pieces 
+ */
 	if (a && b && sb) {
 		Pt z;
 		if (flags&ASF_COUNT) {
 			/* Not implimented */
 		}
 		else {
-			if (( z = asa(a,sa,b,sb,flags ))
+			if (( z = asa(a,sa,b,sb,flags&ASF_REVERSED) )
 				if (sb==sr) aas(b,r,sb);
-				else {
+				else {:
+					
 				}
 		}
 	}
