7b065f4bc27e39bf0d80e446340d8de130e62c10
wspvlv <wspvlv@gmail.com>
2021-12-18 00:57:15 +0300

    ^

--- src/array/ara.c
+++ src/array/ara.c
@@ -1,4 +1,18 @@
 #include <wc/array.h>
+#include <wc/error.h>
+
+
+
+#ifndef mal
+#	include <stdlib.h>
+#	define mal malloc 
+#endif
+#ifndef mfr
+#	include <stdlib.h>
+#	define mfr free
+#endif
+
+
 
 void ara(
 	const void* restrict const a,
@@ -13,9 +27,12 @@ void ara(
  *	2. Make sure size of a haystack is specified, otherwise return ERZERO.
  *	3. If ASF_COUNT flag is set, replace every occurance of `*b` with `*r`:
  *	4. Else, get the pointer to the first/last occurance of `*b`
- *		4a. Check if `sb` and `sr` are equal; we can just assign `r` to
- *		`b` if they are.
- *		4b. Otherwise, split `a` into 3 pieces 
+ *		4a. Check if `sr` <= `sb`; we can just assign `r` to `b` if it is. 
+ *		Note, when `sr` < `sb`, there will be `sr-sb` rightmost bytes remaining
+ *		from `*a`, but you need to treat the array as it has shrunk, and those
+ *		bytes are out of its scope. 
+ *		4b. Otherwise, create an intermediate buffer `_a`
+ *		4c.
  */
 	if (a && b && sb) {
 		Pt z;
@@ -23,19 +40,24 @@ void ara(
 			/* Not implimented */
 		}
 		else {
-			if (( z = asa(a,sa,b,sb,flags&ASF_REVERSED) )
-				if (sb==sr) aas(b,r,sb);
+			if (( z = asa(a,sa,b,sb,flags&ASF_REVERSED) )) {
+#if WL_WRITE_UNALLOC
+				aas(b,r,sb);
+#else
+#endif
+				if (sr<=sb) aas(b,r,sb);
 				else {
-					mal();
-					
-					s = aas( NULL, (void*)(z+sb), sa-(z+sb) );
-			   		aas(z, r, sr);
-						
+			   		const char* _a = mal(sa+sr-sb);
+					aas(_a, a, z-(Pt)a); _a += z-(Pt)a;
+					aas(_a, a, z); _a += z;
+					aas(_a, a, sa-z-(z-(Pt)a));
+					mfr((void*)a);
 				}
+			}
 		}
 	}
 #if WL_ERROR
-	else	if (sb) err(ERZERO);
-			else	err(ERNULL);
-#else
+	else	if (sb) err(ara,ERZERO);
+			else	err(ara,ERNULL);
+#endif
 }
