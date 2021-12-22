e46ff5b46a5a505b954809766511987dceaa1183
wspvlv <wspvlv@gmail.com>
2021-12-23 02:07:28 +0300

    Fixed a faulty code for looped replacement

--- src/array/arb.c
+++ src/array/arb.c
@@ -3,33 +3,54 @@
 
 
 
-/* Array Replace Bit */
+/* Array Replace Byte */
 /* Replace all occurrences of byte `b` with `r` in array `a` */
-void arb(
+U8 arb(
 	const void* restrict const a,
-	const As sa,
+	As sa,
 	const char b,
 	const char r,
 	const Asf flags
 ) {
+/*	Haystack cannot be NULL
+ *	check for flags
+ *		a)	ASF_COUNT flag will replace every occurance of `b`
+ *			1.	Make sure haystack size is specified here
+ *			2.	Haystack gets offset to the next occurance of the needle, until
+ *				no needles are left
+ *			3.	For every needle, 
+ *				(i)		assign replacement byte to the needle pointers
+ *				(ii)	incriment haystack pointer
+ *				(iii)	update the haystack size
+ *				(iv)	incriment replacement count/indicator
+ *		b)	ASF_REVERSE flag will replace first occurance of `b`
+ *		c)	ASF_NORMAL flag will replace last occurance of `b`
+ *			1.	Find first/last occurance, replace it, and incriment rci
+ */
+	U8 rci = 0;
+
 	if (a) {
-		Pt z;
+		const char* _a;
 		/* ASF_COUNT will replace all needles */
 #if WL_ERROR
 		if (flags&ASF_COUNT)
 			if (sa)
-				while (( z = asb(a,sa,b,0) )) {
-					*(char*)z = r; z++;
+				while (( _a = asb(a,sa,b,0) )) {
+					*_a = r;
+					sa -= (_a++)-a; rci++; 
 				}
 			else err(arb,ERZERO);
 #else
 		if (flags&ASF_COUNT && sa)
-			while (( z = asb(a,sa,b,0) )) {
-				*(char*)z = r; z++;
+			while (( a = asb(a,sa,b,0) )) {
+				*_a = r;
+				sa -= (_a++)-a; rci++;
 			}
 #endif
 		/* ASF_REVERSE will replace the last needle, and ASF_NORMAL the first */
-		else if (( z = asb(a,sa,b,flags&WL_ASF_REVERSED) )) *(char*)z = r;
+		else if (( _a = asb(a,sa,b,flags&WL_ASF_REVERSED) )) { *(char*)z = r; rci++ }
 	}
 	else err(arb,ERNULL);
+
+	return rci;
 }
