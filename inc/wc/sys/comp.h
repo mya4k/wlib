0982e24b98533807dcc9b7d7eea6754ba90c4611
wspvlv <wspvlv@gmail.com>
2022-03-20 20:24:25 +0300

    Compilers not recognized, are now WL_CM_OTHER + GCC-like compiler detection + Attributes

--- inc/wc/sys/comp.h
+++ inc/wc/sys/comp.h
@@ -1,26 +1,36 @@
-#ifndef WC_SYS_COMP_H
-#define WC_SYS_COMP_H
+#ifndef WL_SYS_COMP_H
+#define WL_SYS_COMP_H
 
 
 
-#define WC_CM_MSVC			1	/* Microsoft Visual C++ */
-#define WC_CM_CLANG			2	/* Clang */
-#define WC_CM_GCC			3	/* GCC */
-#define WC_CM_MINGW			4	/* MinGW */
+#define WL_CM_MSVC			1	/* Microsoft Visual C++ */
+#define WL_CM_CLANG			2	/* Clang */
+#define WL_CM_GCC			3	/* GCC */
+#define WL_CM_MINGW			4	/* MinGW */
+#define WL_CM_OTHER			-1
 
 
 
-#ifndef WC_CM
+#ifndef WL_CM
 #	if		defined(_MSC_VER)
-#		define WC_CM WC_CM_MSVC
+#		define WL_CM WL_CM_MSVC
 #	elif	defined(__clang__)
-#		define WC_CM WC_CM_CLANG
+#		define WL_CM WL_CM_CLANG
+#		define WL_CLANG_ATTR	1
 #	elif	defined(__GNUC__)
-#		define WC_CM WC_CM_GCC
+#		define WL_CM WL_CM_GCC
+#		define WL_GNU_ATTR		1
 #	elif	defined(__MINGW32__) ||\
 			defined(__MINGW64__)
-#		define WC_CM WC_CM_MINGW
+#		define WL_CM WL_CM_MINGW
+#		define WL_MINGW_ATTR	1
+#	else
+#		define WL_CM WL_CM_OTHER	
 #	endif
+#endif
+
+#if WL_CM==WL_CM_GCC || WL_CM==WL_CM_CLANG || WL_CM==WL_CM_MINGW
+#define WL_GCCLIKE	1
 #endif
 
 
