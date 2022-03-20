9be061b934d28b055afd9606ce6c80cadcd09c2b
wspvlv <wspvlv@gmail.com>
2022-03-20 20:02:20 +0300

    Attributes compatibility

--- inc/wc/core.h
+++ inc/wc/core.h
@@ -4,6 +4,7 @@
 
 
 #include <wc/sys/lang.h>
+#include <wc/sys/comp.h>
 
 
 
@@ -70,6 +71,72 @@
 #	undef	restrict
 #	define	restrict
 #endif
+
+#if WL_GCCLIKE
+#define likely(cond)	__glibc_likely((long)cond)	
+#define unlikely(cond)	__glibc_unlikely((long)cond)
+#else
+#define likely(cond)	(cond)
+#define unlikely(cond)	(cond)
+#endif
+
+#if WL_C_ATTR
+#	if !defined(nodiscard) && __has_c_attribute(nodiscard)
+#	define nodiscard		[[nodiscard]]
+#	endif
+
+#	if WL_GNU_ATTR
+#		if !defined(nonnull) && __has_c_attribute(gnu::nonnull)
+#		define nonnull(...)		[[gnu::nonnull(__VA_ARGS__)]]
+#		endif
+
+#		if !defined(returns_nonnull) && __has_c_attribute(gnu::returns_nonnull)
+#		define returns_nonnull	[[gnu::returns_nonnull]]
+#		endif
+#	endif
+#endif
+
+#if WL_CPP_ATTR
+#	if !defined(nodiscard) && WL_LG_CPP < WL_VR_CPP17
+#	define nodiscard		[[nodiscard]]
+#	endif
+
+#	if WL_GNU_ATTR
+#		if !defined(nonnull)
+#		define nonnull(...)		[[gnu::nonnull(__VA_ARGS__)]]
+#		endif
+
+#		if !defined(returns_nonnull)
+#		define returns_nonnull	[[gnu::returns_nonnull]]
+#		endif
+#	endif
+#endif
+
+#if (WL_GNU_ATTR || WL_CLANG_ATTR || WL_MINGW_ATTR) && !(__STRICT_ANSI__) && defined(__glibc_has_attribute)
+#	if !defined(nodiscard) && __glibc_has_attribute(warn_unused_result)
+#	define nodiscard		__attribute__((warn_unused_result))
+#	endif
+
+#	if !defined(nonnull) && __glibc_has_attribute(warn_unused_result)
+#	define nonnull(...)		__attribute__((nonnull(__VA_ARGS__)))
+#	endif
+
+#	if !defined(returns_nonnull) && __glibc_has_attribute(warn_unused_result)
+#	define returns_nonnull	__attribute__((returns_nonnull))
+#	endif
+#endif
+
+#	if !defined(nodiscard)
+#	define nodiscard
+#	endif
+
+#	if !defined(nonnull)
+#	define nonnull(X)
+#	endif
+
+#	if !defined(returns_nonnull)
+#	define returns_nonnull
+#	endif
 
 
 
