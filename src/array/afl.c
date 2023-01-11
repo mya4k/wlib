c75295b39744ac069753430e49a4a13383c43df7
wspvlv <wspvlv@gmail.com>
2023-01-11 16:12:36 +0300

    Optimized `afla` and `aflv`

--- src/array/afl.c
+++ src/array/afl.c
@@ -5,117 +5,140 @@
 
 #define _min2(x,y)	((x)<=(y) ? (x) : (y))
 
-const void* afla(
+
+
+/* Array fill array*/
+void afla(
 			char* arr1,			U32 len1,
 	const	char* arr2, const	U32 len2
 ) {
-	/* Check for NULLs and zeros */
-	if (likely(arr2!=NULL && len1>0 && len2>0)) {
-		/* If destination is NULL, it needs to be allocated */
-		if (unlikely(arr1 == NULL)) arr1 = mal(len1);
-		if (likely(arr1 != NULL)) {	/* `malloc` can fail */
-			/* Preserve return value */
-			const void* const _arr1 = arr1;
-			/* Fill with `arr2` until len1 < len2 */
-			while (len1>=len2) {
-				/**	`_ano_bare` is the same function as `ano` but it doesn't 
-				 *	check its parameters for holding valid values
-				 */
-				_ano_bare((_Afa_U512*)arr2, len2, (_Afa_U512*)arr1);
-				len1 -= len2;
-				arr1 = arr1+len2;
-			}
-			/* Fill `len1` bytes of `arr2` */
-			_ano_bare((_Afa_U512*)arr2, len1, (_Afa_U512*)arr1);
-			/* Return pointer to destination */
-			return _arr1;
-		}
+	/* Fill with `arr2` until len1 < len2 */
+	while (len1>=len2) {
+		ano(arr2, len2, arr1);
+		len1 -= len2;
+		arr1 = arr1+len2;
 	}
-	return NULL;
 }
 
-#define _FILL64(arr, val)	\
-	_FILL32(arr, val);		\
-	_FILL32(arr+32/sizeof(UMax), val)
-#define _FILL32(arr, val)	\
-	_FILL16(arr, val);		\
-	_FILL16(arr+16/sizeof(UMax), val)
-#define _FILL16(arr, val)	\
-	_FILL8(arr, val);		\
-	_FILL8(arr+8/sizeof(UMax), val)
 
-#if UMB >= 64
-#	define _FILL8(arr, val)	\
-	*(arr) = (val)
-#else
-#	define _FILL8(arr, val)	\
-	*(arr) = (val);			\
-	*(arr+1) = (val)
+#if WL_OPTIMIZE&4 != WL_OPTIMIZE_SIZE
+	always_inline
 #endif
+void aflvsw(char* arr, U8 rem, UMax* val, U8 lenv) {
+#define _val (*val)
+	/* Assuming `lenv` > 4 */
+#if UMB >= 64
+	if (rem&4) {
+		if (lenv > 4) {
+			register U8 mlen = (8*(lenv-4));
+			*(U32*)arr = _val >> mlen;
+			_val = ((UMax)(_val & (((UMax)1<<mlen)-1)) << 32) | *(U32*)arr;
+		}
+		else /* lenv == 3 */ {
+			*(U32*)arr = _val<<8 | _val>>16;
+			_val = *(U32*)arr & 0xFFFFFF;
+		}
+		arr += 4;
+	}
+#endif
+	if (rem&2) {
+		register U8 mlen = (8*(lenv-4));
+		*(U16*)arr = _val >> mlen;
+		_val = ((_val & (((UMax)1<<mlen)-1)) << 16) | *(U16*)arr;
+		arr += 2;
+	}
+	if (rem) {
+		register U8 mlen = (8*(lenv-4));
+		*arr = _val >> mlen;
+		_val = ((_val & (((UMax)1<<mlen)-1)) << 8) | *arr;
+		arr++;
+	}
+}
 
-const void* aflv(UMax* arr, U32 lena, UMax val, U32 lenv) {
-	/* Check for NULLs and zeros */
-	if (likely(lena>0 && lenv>0)) {
-		/* If destination is NULL, it needs to be allocated */
-		if (unlikely(arr == NULL)) arr = mal(lena);
-		/* `lenv` cannot be larger than the largest scalar type */
-		if (likely(arr != NULL)) {
-			/* Preserve return value */
-			const void* const _arr = arr;
-
-			/*	Try making `val` as close to wordsize as possible, so we need
-				to make less assignments overall
-			 */
-			switch (lenv) {
-				case 1:
-					val |= val<<(CHB);
-					lenv += 1;
-					break;
-				case 2:
-					val |= val<<(2*CHB);
-					lenv += 2;
-					break;
+void aflv(char* restrict arr, U32 lena, UMax val, U8 lenv) {
+	/* Duplicate val to fill the word */
+	switch (lenv) {
+		case 1:
+			val |= val<<CHB;
+			val |= val<<(2*CHB);
 #if	UMB >= 64
-				case 4:
-					val |= val<<(4*CHB);
-					lenv += 4;
-					break;
-#endif
-				default: break;
-			}
-
-			while (likely(lena>=64)) {
-				_FILL64(arr, val);
-				arr += 64/sizeof(UMax);
-				lena -= 64;
-			}
-
-			/* Fill with `arr2` until len1 < len2 */
-			if (unlikely(lena&32)) {
-				_FILL32(arr, val);
-				arr += 32/sizeof(UMax);
-				lena -= 32;
-			}
-			if (unlikely(lena&16)) {
-				_FILL16(arr, val);
-				arr += 32/sizeof(UMax);
-				lena -= 32;
-			}
-#if UMB >= 64
-			if (unlikely(lena)) { _FILL8(arr, val); }
+			val |= val<<(4*CHB);
+			lenv = 8;
 #else
-			if (unlikely(lena&8)) { 
-				_FILL8(arr, val); 
-				arr += 2;
-				lena -= 8;
-			}
-			if (unlikely(lena)) 
-				*arr = val;
+			lenv = 4;
 #endif
+			break;
+		case 2:
+			val |= val<<(2*CHB);
+#if	UMB >= 64
+			val |= val<<(4*CHB);
+			lenv = 8;
+#else
+			lenv = 4;
+#endif
+			break;
+#if UMB >= 64
+		case 4:
+			val |= val<<(4*CHB);
+			lenv = 8;
+			break;
+#endif
+	}
 
-			/* Return pointer to destination */
-			return _arr;
+	/* Align `arr` first */
+	{
+		Pt rem = sizeof(UMax) - (Pt)arr % sizeof(UMax);
+		aflvsw(arr, rem, &val, lenv);
+		arr += rem; lena -= rem;
+	}
+
+	/* Fill words */
+	if (lenv == sizeof(UMax)) {
+		while (lena >= sizeof(UMax)) {
+			*(UMax*)arr = val;
+			arr += sizeof(UMax); lena -= sizeof(UMax);
 		}
 	}
-	return NULL;
+	/* Special behavior when `lenv` < wordsize */
+#if	UMB >= 64
+	else if (lenv == 3) {
+		while (lena >= lenv) {
+			/* 3 */ *arr = val<<40 | val<<16 | val>>8;
+			val = *arr&0xFFFFFFF;
+			arr++; lena -= 8;
+		}	
+	}
+	else if (lenv == 5) {
+	  	while (lena > lenv) {
+			/* 5 */	*arr = val<<24 | val>>16;
+			val = *arr&0xFFFFFFFFFF;
+			arr++; lena -= 8;
+		}
+	}
+	else if (lenv == 6) {
+		while (lena > lenv) {
+			/* 6 */ *arr = val<<16 | val>>32;
+			val = *arr&0xFFFFFFFFFFFF;
+			arr++; lena -= 8;
+		}
+	}
+	else /* if (lenv == 7) */ {		
+		while (lena > lenv) {
+			/* 7 */ *arr = val<<8 | val>>56;
+			val = *arr&0xFFFFFFFFFFFFFF;
+			arr++; lena -= 8;
+		}	
+	}
+#else
+	else /* if (lenv == 3) */ {
+		while (lena >= lenv) {
+			*arr = val<<8 | val>>16;
+			val = *arr&0xFFFFFF;
+			arr++; lena -= 4;
+		}
+	}
+#endif
+
+	/* Fill remaining bytes */
+	aflvsw(arr, lena, &val, lenv);
 }
