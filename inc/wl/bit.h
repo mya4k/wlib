b6bc25b567bb749a36ced7ad7b9ba7e52df80250
wspvlv <wspvlv@gmail.com>
2023-12-31 18:32:35 +0300

    Major changes\n\n1.\tInitials for 'set bit' (s) and 'clear bit' (c) replaced with 1 and 0 respectively;\n2.\tAdded  (popcount function;\n3.\tNow bit manipulation functions no longer work on given value, and work on either  or  to in order to make them more fundamental;\n4.\tAdded , ;\n 5.\tAdded implementation of functions through _Generic;

--- inc/wl/bit.h
+++ inc/wl/bit.h
@@ -8,8 +8,8 @@
 
 
 #if !WL_PREFIX
-#	define bs	wl_bs
-#	define bc	wl_bc
+#	define b0	wl_b0
+#	define b1	wl_b1
 #	define br	wl_br
 #	define brs	wl_brs
 #	define bt	wl_bt
@@ -19,6 +19,7 @@
 
 #	define clz	wl_clz
 #	define ctz	wl_ctz
+#	define pop	wl_pop
 #endif
 
 
@@ -29,29 +30,26 @@ extern wl_U8f wl__ctz_table[64];
 
 /**
  * \brief	Bit Set
- * \def		wl_bs(x,n)
- * \param	x	Pool of bits
+ * \def		b1(n)
  * \param	n	Number of the bit to be set
  * 
- * Sets the bit `n` to 1 in `x`. 
- * This function does not override the value of `x`
+ * Returns a empty bit-string, but with \a n bit set
  */
-#define wl_bs(x,n)	(((UMax)1<<(n)) | (x))
+#define wl_b1(n)	(((UMax)1<<(n)))
 
 /**
  * \brief	Bit Clear
- * \def		wl_bc(x,n)
+ * \def		b0(n)
  * \param	x	Pool of bits
  * \param	n	Number of the bit to be cleared
  * 
- * Sets the bit `n` to 0 in `x`. 
- * This function does not override the value of `x`
+ * Returns a bit-string full of set bits, but with \a n bit clear
  */
-#define wl_bc(x,n)	(~((UMax)1<<(n)) & (x))
+#define wl_b0(x,n)	(~wl_b1(n))
 
 /**
  * \brief	Bit Read
- * \def		wl_br(x,n)
+ * \def		br(x,n)
  * \param	x	Pool of bits
  * \param	n	Number of the bit to be read
  * 
@@ -61,7 +59,7 @@ extern wl_U8f wl__ctz_table[64];
 
 /**
  * \brief	Bit Read String
- * \def		wl_brs(x,n,l)
+ * \def		brs(x,n,l)
  * \param	x	Pool of bits
  * \param	n	Number of the bit from where to start reading
  * \param	l	Length of the string to be read in bits
@@ -72,7 +70,7 @@ extern wl_U8f wl__ctz_table[64];
 
 /**
  * \brief	Bit Toggle
- * \def		wl_bt(x,n)
+ * \def		bt(x,n)
  * \param	x	Pool of bits
  * \param	n	Number of the bit to be toggled
  * 
@@ -83,7 +81,7 @@ extern wl_U8f wl__ctz_table[64];
 
 /**
  * \brief	Bit Convert Trailing zeros
- * \def		wl_bct0(x)
+ * \def		bct0(x)
  * \param	x	Pool of bits
  * 
  * Converts trailing zeros to one
@@ -93,7 +91,7 @@ extern wl_U8f wl__ctz_table[64];
 
 /**
  * \brief	Bit Least Significant 1
- * \def		wl_bls1(x)
+ * \def		bls1(x)
  * \param	x	Pool of bits
  * 
  * Extracts the least significant set bit
@@ -111,6 +109,23 @@ extern wl_U8f wl__ctz_table[64];
  */
 #define wl_bmc(a,b,m)	((a & m) | (b & ~m))
 
+/**
+ * \brief	Trailing Ones
+ * \def		wl_bt1(n)
+ * \param	n	Number of trailing ones
+ *
+ * Returns an empty bit-string, but with n trailing set bits
+ */
+#define wl_bt1(n)	(wl_b1(n)-1)
+
+/**
+ * \brief	Leading Ones
+ * \def		wl_bl1(n)
+ * \param	n	Number of leading ones
+ *
+ * Returns an empty bit-string, but with n trailing set bits
+ */
+#define wl_bl1(n)	(~wl_b1((sizeof(n)*WL_CHB) -n) -1)
 
 
 /**
@@ -122,7 +137,17 @@ extern wl_U8f wl__ctz_table[64];
  * this branchless
  */
 #if	WL_BUILTIN_CTZ
-#	define wl_ctz(x)	__builtin_ctzll(x)
+#	if WL__GENERIC
+#		define wl_ctz(x) _Generic((x),				\
+			default:			__builtin_ctz(x),	\
+			long:				__builtin_ctzl(x),	\
+			long long:			__builtin_ctzll(x),	\
+			unsigned long:		__builtin_ctzl(x),	\
+			unsigned long long:	__builtin_ctzll(x)	\
+		)(x)
+#	else
+#		define wl_ctz(x)	__builtin_ctzll(x)
+#	endif
 #else
 #	if		UMB == 64
 #		define WL__DEBRUIJN	0x218A392CD3D5DBF
@@ -138,15 +163,47 @@ extern wl_U8f wl__ctz_table[64];
 /**
  * \brief	Count leading zeros
  * \def		wl_clz(x)
- * \param	x
+ * \param	x  
  * 
+ * Counts the number of leading zeros in word \a x
  */
 #if	WL_BUILTIN_CLZ
-#	define wl_clz(x)	__builtin_clzll(x)
+#	if WL__GENERIC
+#		define wl_clz(x) _Generic((x),				\
+			default:			__builtin_clz(x),	\
+			long:				__builtin_clzl(x),	\
+			long long:			__builtin_clzll(x),	\
+			unsigned long:		__builtin_clzl(x),	\
+			unsigned long long:	__builtin_clzll(x)	\
+		)(x)
+#	else
+#		define wl_clz(x)	__builtin_clzll(x)
+#	endif
 #else
-extern inline U8f wl_clz(register UMax x);
+	extern inline U8 wl_clz(register UMax x);
 #endif
 
-
+/**
+ * \brief	Popcount
+ * \def		wl_pop(x)
+ * \param	x
+ * 
+ * Performs a Hamming's weight calculation on x 
+ */
+#if	WL_BUILTIN_POPCOUNT
+#	if WL__GENERIC
+#		define wl_pop(x) _Generic((x),					\
+			default:			__builtin_popcount(x),	\
+			long:				__builtin_popcountl(x),	\
+			long long:			__builtin_popcountll(x),\
+			unsigned long:		__builtin_popcountl(x),	\
+			unsigned long long:	__builtin_popcountll(x)	\
+		)(x)
+#	else
+#		define wl_pop(x)	__builtin_popcountll(x)
+#	endif
+#else
+	extern inline U8 wl_pop(register UMax x);
+#endif
 
 #endif
