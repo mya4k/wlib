4728ca2be6fd2f351804e93d7d01fcf25864725e
wspvlv <wspvlv@gmail.com>
2024-01-06 21:27:57 +0300

    rt2u implemented, slight edits to rt2d

--- src/math/rt.c
+++ src/math/rt.c
@@ -8,7 +8,7 @@ double rt2d(double power) {
 	double a;
 	U8 i;
 
-	for (i = 0; i<32 && x*x != power; i++) {
+	for (i = 0; x*x != power; i++) {
 		/* We use Bakhshali method */
 		a = (power - x*x) / (2*x);
 		
@@ -18,8 +18,33 @@ double rt2d(double power) {
 
 		/*	Make sure we aren't stuck in an infinite loop going through several
 			values */
-		if (x1 == x || x2 == x) return x;
+		if_unlikely (x1 == x || x2 == x) return x;
 	}
 
 	return x;
+}
+
+
+
+U32 rt2u(U64 power) {
+	if_unlikely (power == 0) return 0;
+	if_unlikely (power < 4) return 1;
+
+	U64 guess, guessLow, guessHigh;
+
+	/*	For power <= 64, `lg2u(power)` is closer or as close to 
+		`rt2u(power)` as `1 << (lg2u(power)/2)`  */
+	if_likely (power <= 64) guess = lg2u(power);
+	else guess = 1 << (lg2u(power)/2);
+
+	guessLow = guess/2; guessHigh = guess*2;
+
+	for (;;) {
+		if_likely (power >= sq(guess)) {
+			if_unlikely (power < sq(guess+1)) return guess;
+			else guessLow = guess;
+		}
+		else guessHigh = guess;
+		guess = av2(guessLow, guessHigh);
+	}
 }
