a1075603903e250749df61b86880cac839af289a
wspvlv <wspvlv@gmail.com>
2024-01-21 23:04:27 +0300

    Const time sl for const at compile time strings

--- inc/wl/string.h
+++ inc/wl/string.h
@@ -1,21 +1,36 @@
-#ifndef WL_STRING_H
-#define WL_STRING_H
+#ifndef WL__STRING_H
+#define WL__STRING_H	1
 
 
 
-#include <wl/core.h>
+#include <wl/types.h>
 
 
 
 #if !WL_PREFIX
 #	define sl		wl_sl
-
+#	define _sl		wl__sl
 #endif
 
 
 
+#define wl_sl(string) (((char*)(string))[sizeof(string)] ? wl__sl(string) : sizeof(string)-1)
+
+
+
+typedef char wl_St[];
+/**
+ * \brief	Null-terminated String type
+ * \typedef	wl_NSt
+ * 
+ * Data structure storing an array of `char`s
+ */
+typedef char wl_NSt[];
+
+
+
 /* String length */
-extern wl_U16 wl_sl(const char* restrict const string);
+extern wl_U16 wl__sl(const char* restrict const string);
 
 
 
